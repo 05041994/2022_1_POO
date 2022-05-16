b1f623a5b6d50694ce39ae139216b3377e4277ed
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-05-16 15:43:04 -0300

    Create ex2.cs

---
+++ lista 2/ex2.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Qual é o seu nome ?");
+      string s = Console.ReadLine();
+    Console.WriteLine("Seja bem Vindo ao C# ," + s);
+  }
+}
