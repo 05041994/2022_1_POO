2d8d859a9ba4f443f810dc6261b6cfff2eb92c03
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-05-21 16:38:19 -0300

    Create ex1.cs

---
+++ Lista01/ex1.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Digite o Seu Primero Nome:");
+    string x1 = Console.ReadLine();
+      
+    Console.WriteLine("Bem-Vindo ao C#," + x1 );
+  
+  }
+}
