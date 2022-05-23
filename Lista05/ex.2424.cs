f3981ba0461d3f29780ec764c616b5d21d05ebab
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-05-22 22:27:27 -0300

    Create ex.2424.cs

---
+++ Lista05/ex.2424.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Program {
+
+  public static void Main(string[] args) {
+
+      
+    string x1 = Console.ReadLine();
+
+    string[] a = x1.Split(' ');
+
+    int c1 = int.Parse(a[0]);
+    int c2 = int.Parse(a[1]);
+
+    if (0 <= c1 <= 432 && 0 <= c2  <= 936)
+    {
+      Console.WriteLine("dentro");
+    }
+    else{
+      Console.WriteLine("fora");
+    }
+    
+  }
+}
