789540dfc255876fbbe5f5ed486fbd2838b37790
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-06-09 17:04:19 -0300

    Create Ex.1.cs

---
+++ Lista06/Ex.1.cs
@@ -0,0 +1,12 @@
+using System;
+
+  public class Program{
+    
+    public static void Main(string[] args){
+
+    for(int par = 2; par <= 100; par += 2){
+      Console.WriteLine(par);
+    }
+          
+  }
+}
