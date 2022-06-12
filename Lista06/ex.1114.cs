dcff0a0dbe55f23b4fcc3b4449e14eebf3a35613
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-06-12 14:55:55 -0300

    Create ex.1114.cs

---
+++ Lista06/ex.1114.cs
@@ -0,0 +1,17 @@
+using System;
+
+  public class Program{
+    
+    public static void Main(string[] args){
+    
+    for(int x =1; x <= x; x++){
+      int y = int.Parse(Console.ReadLine());
+      if(y != 2002){
+        Console.WriteLine("Senha Invalida");
+      }
+      else {
+        Console.WriteLine("Acesso Permitido");break;
+      }
+    }
+  }
+}
