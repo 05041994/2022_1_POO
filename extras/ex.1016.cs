63fd13794d420a60bc4f4e74f844ef1c4f38bc1d
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-06-17 22:32:43 -0300

    Create ex.1016.cs

---
+++ extras/ex.1016.cs
@@ -0,0 +1,15 @@
+using System;
+
+  public class Program{
+    
+    public static void Main(string[] args){
+    int s1;
+    
+    s1 = int.Parse(Console.ReadLine());
+
+    int total = s1 * 2;
+
+    Console.WriteLine($"{total:0} minutos");
+      
+  }
+}
