08f141c22b4de997abb514d0992a3d9115c3dce1
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-06-24 16:02:54 -0300

    Update ex.1094.cs

--- Lista06/ex.1094.cs
+++ Lista06/ex.1094.cs
@@ -4,9 +4,9 @@ using System;
     
     public static void Main(string[] args){
     
-      int x,total1,total2,total3,soma;  // Inicial
-      x = total1 = total2 = total3 = soma = 0;
-      double p1,p2,p3;
+      int x,total1,total2,total3;  // Inicial
+      x = total1 = total2 = total3 =  0;
+      double p1,p2,p3,soma;
       x = int.Parse(Console.ReadLine());
 
       for(int x1 = 1; x1 <=x ; x1++){
@@ -21,15 +21,15 @@ using System;
           else if(v2 == 'R'){
           total2 += v1;          
           }
-          else{
+          else if(v2 == 'S'){
           total3 += v1;
           }        
         }
       }
   soma = total1 + total2 + total3;
-  p1 = 
-  p2 = 
-  p3 = 
+  p1 = (total1 * 100) / soma;
+  p2 = (total2 * 100) / soma;
+  p3 = (total3 * 100) / soma;
   Console.WriteLine($"Total: {soma:0} cobaias");  
   Console.WriteLine($"Total de coelhos: {total1:0}");
   Console.WriteLine($"Total de ratos: {total2:0}");
@@ -39,4 +39,3 @@ using System;
   Console.WriteLine($"Percentual de sapos: {p3:0.00} %");
   }
 }
-// Não sei fazer porcentagem...
