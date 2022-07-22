fbcd280c81ac59f1fa02146019dac3d6c1496271
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-07-22 15:06:03 -0300

    Update ex.01.cs

--- Lista de Revisão 05/ex.01.cs
+++ Lista de Revisão 05/ex.01.cs
@@ -1,1 +1,17 @@
+using System;
 
+class Program {
+  public static void Main() {
+    double x, x1;
+    Console.WriteLine("Digite dois valores: um em cada linha.");
+    x = double.Parse(Console.ReadLine());
+    x1 = double.Parse(Console.ReadLine());
+    Console.WriteLine(Menor(x,x1));
+  }
+  public static double Menor(double y, double y1){
+    if(y < y1)
+      return y;
+    else
+      return y1;
+  }
+}
